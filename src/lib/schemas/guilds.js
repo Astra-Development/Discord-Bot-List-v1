57cdfed62209954ec9f31f425fc1a5fbe9b468bc
LuckyTarget <>
2021-05-18 10:48:20 +0000

    update

--- src/lib/schemas/guilds.js
+++
@@ -1,6 +0,0 @@
-const { Client } = require('klasa');
-
-Client.defaultGuildSchema
-  .add('autoAssignRole', 'role')
-  .add('customCommands', 'any', { array: true, configurable: false })
-  .add('roles', folder => folder.add('muted', 'role'));
