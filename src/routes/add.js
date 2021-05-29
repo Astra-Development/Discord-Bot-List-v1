4faa05368490738e5e08428ec76b8be5734a2e11
yasmijn <59023524+YasmijnVels@users.noreply.github.com>
2021-05-29 22:39:09 +0200

    removing the blacklist system

--- src/routes/add.js
+++ src/routes/add.js
@@ -1,31 +1,17 @@
 const { Router } = require("express");
 const { auth } = require('@utils/discordApi');
-//const Users = require("@models/users");
-//const blacklist = require("@root/config.json");
+
 const { web: { recaptcha_v2: { site_key } }, bot_options: { bot_tags, max_summary_length } } = require("@root/config.json");
 
 const route = Router();
 
-
 route.get("/", auth, async (req, res) => {
   res.render("add", {
-         bot_tags,
-         max_summary_length,
-         site_key,
-         req
-       }) 
-//   const bot = await Users.findOne({ userid: req.user.id }, { _id: false })
-//   if (bot.blacklisted) {
-//    return res.render("blacklisted", {req})
-    
-//   }else{
-//   res.render("add", {
-//     bot_tags,
-//     max_summary_length,
-//     site_key,
-//     req
-//   }) 
-// }
+    bot_tags,
+    max_summary_length,
+    site_key,
+    req
+  })
 });
 
 module.exports = route;
