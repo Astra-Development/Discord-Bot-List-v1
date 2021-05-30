10352f6f6760fe6c52165a24187f4199f9abfe19
yasmijn <59023524+YasmijnVels@users.noreply.github.com>
2021-05-30 14:49:39 +0200

    added login message to discord

--- src/routes/api/callback.js
+++ src/routes/api/callback.js
@@ -1,21 +1,25 @@
 const { Router } = require("express");
 const passport = require('passport');
-const { Command } = require('klasa');
 const { MessageEmbed } = require("discord.js");
 const { server } = require("@root/config.json");
+const perms1 = require("@root/config.json");
 const route = Router();
 
 route.get("/", passport.authenticate('discord', {
     failureRedirect: '/'
 }), function(req, res) {
-    
-    res.redirect(req.session.url || "/me");
-    let embed = new MessageEmbed()
-    .setTitle('Login Detected')
-    .setThumbnail(req.user.avatarURL)
-    .setColor('BLUE')
-    .addField('User logged in', `<@${req.user.id}>`);
-    req.app.get('client').channels.cache.get(server.website_logs).send(embed)
+    if(!server.website_logs){
+        res.redirect(req.session.url || "/me");
+    }else{
+        res.redirect(req.session.url || "/me");
+        let embed = new MessageEmbed()
+        .setAuthor(`${req.user.username}#${req.user.discriminator}`, `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=256`)
+        .setColor('BLUE')
+        .setThumbnail(`https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=256`)
+        .setDescription(`<@${req.user.id}>, Welcome to our Bot List!`);
+        req.app.get('client').channels.cache.get(server.website_logs).send(embed)
+    }
+
 });
 
 module.exports = route;
