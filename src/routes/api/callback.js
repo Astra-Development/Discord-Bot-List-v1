bad7ed282d628b0cdf6f6a41ae8ff322b055922b
yasmijn <59023524+YasmijnVels@users.noreply.github.com>
2021-05-30 13:13:28 +0200

    user login added

--- src/routes/api/callback.js
+++ src/routes/api/callback.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const passport = require('passport');
-
+const { MessageEmbed } = require("discord.js");
+const { server } = require("@root/config.json");
 const route = Router();
 
 route.get("/", passport.authenticate('discord', {
@@ -8,6 +9,11 @@ route.get("/", passport.authenticate('discord', {
 }), function(req, res) {
     
     res.redirect(req.session.url || "/me");
+    let embed = new MessageEmbed()
+    .setTitle('login detected')
+    .setColor('BLUE')
+    .addField('User logged in', `<@${req.user.id}>`);
+    req.app.get('client').channels.cache.get(server.website_logs).send(embed)
 });
 
 module.exports = route;
