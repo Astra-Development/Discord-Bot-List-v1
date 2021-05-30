1305dbda7d8d5764b32a44552d8ffdbdf282f47e
LuckyTarget <>
2021-05-30 10:14:24 +0000

    update

--- src/routes/certify.js
+++ src/routes/certify.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const { auth } = require('@utils/discordApi')
 const Bots = require("@models/bots");
+const { web: {recaptcha_v2: {site_key}}} = require("@root/config.json");
 
 const route = Router();
 
-route.get("/", auth, async (req, res) => {
-    res.render("certify", {req})
+route.get("/:id",auth, async (req, res) => {
+  let bots = await Bots.findOne({botid: req.params.id})
+    res.render("certify", {
+      bots,
+      req,
+      site_key
+    })
 });
 
 module.exports = route;
