84562428b66e84752c3be44cbc2f774d8dbe18ce
yasmijn <59023524+YasmijnVels@users.noreply.github.com>
2021-05-30 14:49:04 +0200

    added logout message to discord

--- src/routes/logout.js
+++ src/routes/logout.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
-
+const { MessageEmbed } = require("discord.js");
+const { server } = require("@root/config.json");
 const route = Router();
 
 route.get("/", async (req, res) => {
-  req.logout();
-  res.redirect(`/`);
+        if(!server.website_logs){
+          req.logout();
+          res.redirect(`/`);
+        }else{
+        let embed = new MessageEmbed()
+        .setAuthor(`${req.user.username}#${req.user.discriminator}`, `https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=256`)
+        .setColor('BLUE')
+        .setThumbnail(`https://cdn.discordapp.com/avatars/${req.user.id}/${req.user.avatar}.png?size=256`)
+        .setDescription(`<@${req.user.id}>, Goodbye`);
+        req.app.get('client').channels.cache.get(server.website_logs).send(embed)
+            req.logout();
+            res.redirect(`/`);
+        }
 });
 
 module.exports = route;
