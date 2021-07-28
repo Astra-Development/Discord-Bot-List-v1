4ce5af540c65553c780ca0bae1aa8778e556af89
LuckyDev <39243722+LuckyTarget@users.noreply.github.com>
2021-07-28 10:12:18 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,5 @@
-@ECHO off
-Title Discord Bot Lists
-echo Starting Backend Server .
-echo Starting Frontend Server..
-echo Discord Bot List By Astra Development...
-:main
-npm start
-goto main
-PAUSE
-
+@echo off
+color c
+title Welcome to Astra Development
+node index.js
+explorer "https://discord.gg/sQQFSnQhdt"
